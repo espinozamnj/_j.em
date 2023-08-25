e11eeb5795d028cb2ea4462de9149fb380a94ac6
espinozamnj <1052179@senati.pe>
2023-08-25 15:09:22 -0500

    all

--- links/links.js
+++ links/links.js
@@ -13,54 +13,52 @@
         'app-link',
         location.origin + '/_cdn_/getlogin',
     ]
-    function new_date() {
-        return new Date().getTime()
-    }
-    fetch(xw + 'bks-version.json?' + new_date()).then(
-        function (response) {
-            response.text().then(function(query){
-                let localData = 'last-version-links'
-                let json = JSON.parse(query)
-                let vsn = json['v']
-                let require_reload = false
-                if (localStorage[localData] == undefined || localStorage[localData] != vsn.toString()) {
-                    localStorage[localData] = vsn
-                    require_reload = true
-                    setTimeout(function() {
-                        let alrt = document.createElement('div')
-                        alrt.innerText = 'cache update'
-                        alrt.classList.add('cache-alert')
-                        document.querySelector('.root').appendChild(alrt)
-                        alrt.classList.add('alert-view')
-                        setTimeout(function(){
-                            alrt.parentElement.removeChild(alrt)
-                        }, 3e3)
-                    }, 1e3)
-                }
-                let s, js = typeof(rload) == 'undefined'
-                if (js) {
-                    s = document.getElementById('scripter')
-                } else {
-                    s = document.getElementsByClassName('list')[0]
-                }
-                for (let i = 0; i < ssrc.length; i++) {
-                    let e, r = ssrc[i], sr
-                    js ? e = document.createElement('script') : e = document.createElement('a')
-                    // dir_project
-                    r = r.replace('@', location.origin + '/_j.em/')
-                    r.startsWith('http') ? sr = r + '.js' : sr = 'bookmaps/' + r + '.js'
-                    require_reload ? sr = sr + '?' + new_date() : sr = sr
-                    if (js) {
-                        e.setAttribute('src', sr)
-                    } else {
-                        e.setAttribute('href', sr)
-                        e.innerText = sr
-                    }
-                    s.appendChild(e)
-                }
-            })
+    function newTime() {return new Date().getTime()}
+
+    let sourceVersion = document.createElement('script')
+    document.head.appendChild(sourceVersion)
+    sourceVersion.addEventListener('load', function() {
+        let localData = 'last-version-links'
+        let require_reload = false
+        if (localStorage[localData] == undefined || localStorage[localData] != book_txt_version.toString()) {
+            localStorage[localData] = book_txt_version
+            require_reload = true
+            setTimeout(function() {
+                let alrt = document.createElement('div')
+                alrt.innerText = 'cache update'
+                alrt.classList.add('cache-alert')
+                document.querySelector('.root').appendChild(alrt)
+                alrt.classList.add('alert-view')
+                setTimeout(function(){
+                    alrt.parentElement.removeChild(alrt)
+                }, 3e3)
+            }, 1e3)
         }
-    ).catch(function(error) {
-        console.log('fetch request failed:' + error.message);
+        let s, js = typeof(rload) == 'undefined'
+        if (js) {
+            s = document.getElementById('scripter')
+        } else {
+            s = document.getElementsByClassName('list')[0]
+        }
+        for (let i = 0; i < ssrc.length; i++) {
+            let e, r = ssrc[i], sr
+            js ? e = document.createElement('script') : e = document.createElement('a')
+            // dir_project
+            r = r.replace('@', location.origin + '/_j.em/')
+            r.startsWith('http') ? sr = r + '.js' : sr = 'bookmaps/' + r + '.js'
+            require_reload ? sr = sr + '?' + new_date() : sr = sr
+            if (js) {
+                e.setAttribute('src', sr)
+            } else {
+                e.setAttribute('href', sr)
+                e.innerText = sr
+            }
+            s.appendChild(e)
+        }
+        delete window['book_txt_version']
     })
+    sourceVersion.addEventListener('error', function() {
+        alert('Error to get data')
+    })
+    sourceVersion.src = xw + 'book-txt-version.js?' + newTime()
 })()
