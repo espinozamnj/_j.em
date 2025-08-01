d9c6b7ca479007993474cf6255db48b7130d3b5c
espinozamnj <espinozamnj@gmail.com>
2025-08-01 16:44:41 -0500

    all

--- off/save/pa55/connect-pa55.js
+++ off/save/pa55/connect-pa55.js
@@ -1,25 +1,58 @@
 (function() {
-  let loc = location.hash.replace('#', '')
   let box = document.querySelector('.console')
-  if (loc != '' && loc.split('-').length == 2) {
+  let input = document.querySelector('.ipt')
+  let stored = localStorage.getItem('link-pa55')
+
+  if (stored) {
+    input.value = '0:'
+  } else {
+    input.value = '00'
+  }
+
+  document.querySelector('#send').addEventListener('submit', function(e) {
+    e.preventDefault()
+    let val = input.value.toLowerCase().trim()
+
+    if (val === '::') {
+      localStorage.removeItem('link')
+      box.innerHTML = '<span class="t-yellow">Forgot!</span>'
+      setTimeout(() => location.href = location.pathname, 1000)
+      return
+    }
+
+    if (val === '0:' && stored) {
+      connect(stored, false)
+    } else {
+      let useForStorage = val
+      let clean = val.replace(/^:|:$/g, '')
+      location.hash = clean
+      if ((val.startsWith(':') || val.endsWith(':')) && clean) {
+        sessionStorage.setItem('_pendingStore', useForStorage)
+      }
+    }
+  })
+
+  let loc = location.hash.replace('#', '')
+  if (loc && loc.split('-').length == 2) {
     let dm = loc.split('-')
-    let js = {
-      tths: function() {return new Date().getTime()},
-      cont: document.querySelector('.js'),
-      strg: `//${dm[0]}.test/public/apps/${dm[1]}/`,
-      csjs: function() {return document.createElement('script')}
-    }
+    let base = `//${dm[0]}.test/public/apps/${dm[1]}/`
     if (location.host.includes('github')) {
-      js.strg = `//${dm[0]}.web.app/apps/${dm[1]}/`
+      base = `//${dm[0]}.web.app/apps/${dm[1]}/`
     }
-    if (dm[0] == ('//')) {
-      js.strg = dm[1]
+    connect(base, true)
+  }
+
+  function connect(base, tryStore) {
+    let js = {
+      tths: function() { return new Date().getTime() },
+      cont: document.querySelector('.js'),
+      strg: base,
+      csjs: function() { return document.createElement('script') }
     }
 
     box.innerHTML = '<span class="t-yellow">Connecting data...</span>'
     let dat = js.csjs()
     dat.setAttribute('src', js.strg + 'data-pa55.js?' + js.tths())
-    
     dat.addEventListener('error', function() {
       box.innerHTML = '<span class="t-red">No data connect</span>'
     })
@@ -33,25 +66,27 @@
         })
         japp.addEventListener('load', function() {
           box.innerHTML = '<span class="t-green">Ready!</span>'
+          if (tryStore) {
+            let pending = sessionStorage.getItem('_pendingStore')
+            if (pending) {
+              localStorage.setItem('link', pending)
+              sessionStorage.removeItem('_pendingStore')
+            }
+          }
           setTimeout(function() {
             if (!!window.initpa55) {
               box.innerHTML = ''
             } else {
               box.innerHTML = '<span class="t-red">Program not initialized</span>'
             }
-          }, 2e3)
+          }, 2000)
         })
         js.cont.appendChild(japp)
-      }, 1e3)
+      }, 1000)
     })
     js.cont.appendChild(dat)
-  } else {
-    document.querySelector('.ipt').value = '00'
-    document.querySelector('#send').addEventListener('submit', function(e) {
-      e.preventDefault()
-      location.hash = document.querySelector('.ipt').value
-    })
   }
+
   window.addEventListener('hashchange', function() {
     location.reload()
   })
